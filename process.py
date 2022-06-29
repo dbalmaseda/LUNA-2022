9c38ec80738a1814e2815587c972ef402e728659
dbalmaseda <62678262+dbalmaseda@users.noreply.github.com>
2022-06-29 20:13:43 +0200

    phase 2 try new final

--- process.py
+++ process.py
@@ -1,23 +1,21 @@
 from typing import Dict
 
-import tensorflow.keras
-from tensorflow.keras.applications import VGG16,ResNet50,EfficientNetB0,InceptionResNetV2
-
 import SimpleITK
 import numpy as np
 from pathlib import Path
 import json
 
+import tensorflow.keras
+from tensorflow.keras.applications import VGG16,ResNet50,EfficientNetB0,InceptionResNetV2
+
+# Enforce some Keras backend settings that we need
+tensorflow.keras.backend.set_image_data_format("channels_first")
+tensorflow.keras.backend.set_floatx("float32")
 
 from data import (
     center_crop_volume,
     get_cross_slices_from_cube,
 )
-
-
-# Enforce some Keras backend settings that we need
-tensorflow.keras.backend.set_image_data_format("channels_first")
-tensorflow.keras.backend.set_floatx("float32")
 
 def clip_and_scale(
     data: np.ndarray,
